0462f16612a973d084bbdbd13e784b470346cf2e
Дмитрий <neelovdima@gmail.com>
2026-04-16 17:05:19 +0300

    Polish Form1 layout spacing and visual consistency

--- App/VPNExclude/Form1.Designer.cs
+++ App/VPNExclude/Form1.Designer.cs
@@ -28,6 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
             topPanel = new Panel();
             topButtonsLayout = new FlowLayoutPanel();
             btnAddDomain = new Button();
@@ -80,11 +82,12 @@
             // topPanel
             // 
             topPanel.Controls.Add(topButtonsLayout);
+            topPanel.BackColor = SystemColors.Control;
             topPanel.Dock = DockStyle.Top;
             topPanel.Location = new Point(0, 0);
             topPanel.Name = "topPanel";
-            topPanel.Padding = new Padding(10, 8, 10, 8);
-            topPanel.Size = new Size(1184, 52);
+            topPanel.Padding = new Padding(12, 8, 12, 8);
+            topPanel.Size = new Size(1200, 48);
             topPanel.TabIndex = 0;
             // 
             // topButtonsLayout
@@ -96,10 +99,10 @@
             topButtonsLayout.Controls.Add(btnRefresh);
             topButtonsLayout.Controls.Add(btnApplyRoutes);
             topButtonsLayout.Dock = DockStyle.Fill;
-            topButtonsLayout.Location = new Point(10, 8);
+            topButtonsLayout.Location = new Point(12, 8);
             topButtonsLayout.Margin = new Padding(0);
             topButtonsLayout.Name = "topButtonsLayout";
-            topButtonsLayout.Size = new Size(1164, 36);
+            topButtonsLayout.Size = new Size(1176, 32);
             topButtonsLayout.TabIndex = 0;
             topButtonsLayout.WrapContents = false;
             // 
@@ -107,9 +110,9 @@
             // 
             btnAddDomain.AutoSize = true;
             btnAddDomain.Location = new Point(0, 0);
-            btnAddDomain.Margin = new Padding(0, 0, 8, 0);
+            btnAddDomain.Margin = new Padding(0, 0, 10, 0);
             btnAddDomain.Name = "btnAddDomain";
-            btnAddDomain.Size = new Size(129, 31);
+            btnAddDomain.Size = new Size(129, 32);
             btnAddDomain.TabIndex = 0;
             btnAddDomain.Text = "Добавить домен";
             btnAddDomain.UseVisualStyleBackColor = true;
@@ -117,10 +120,10 @@
             // btnAddIp
             // 
             btnAddIp.AutoSize = true;
-            btnAddIp.Location = new Point(137, 0);
-            btnAddIp.Margin = new Padding(0, 0, 8, 0);
+            btnAddIp.Location = new Point(139, 0);
+            btnAddIp.Margin = new Padding(0, 0, 10, 0);
             btnAddIp.Name = "btnAddIp";
-            btnAddIp.Size = new Size(104, 31);
+            btnAddIp.Size = new Size(104, 32);
             btnAddIp.TabIndex = 1;
             btnAddIp.Text = "Добавить IP";
             btnAddIp.UseVisualStyleBackColor = true;
@@ -128,10 +131,10 @@
             // btnCheckDomain
             // 
             btnCheckDomain.AutoSize = true;
-            btnCheckDomain.Location = new Point(249, 0);
-            btnCheckDomain.Margin = new Padding(0, 0, 8, 0);
+            btnCheckDomain.Location = new Point(253, 0);
+            btnCheckDomain.Margin = new Padding(0, 0, 10, 0);
             btnCheckDomain.Name = "btnCheckDomain";
-            btnCheckDomain.Size = new Size(130, 31);
+            btnCheckDomain.Size = new Size(130, 32);
             btnCheckDomain.TabIndex = 2;
             btnCheckDomain.Text = "Проверить домен";
             btnCheckDomain.UseVisualStyleBackColor = true;
@@ -139,10 +142,10 @@
             // btnDelete
             // 
             btnDelete.AutoSize = true;
-            btnDelete.Location = new Point(387, 0);
-            btnDelete.Margin = new Padding(0, 0, 8, 0);
+            btnDelete.Location = new Point(393, 0);
+            btnDelete.Margin = new Padding(0, 0, 10, 0);
             btnDelete.Name = "btnDelete";
-            btnDelete.Size = new Size(77, 31);
+            btnDelete.Size = new Size(77, 32);
             btnDelete.TabIndex = 3;
             btnDelete.Text = "Удалить";
             btnDelete.UseVisualStyleBackColor = true;
@@ -150,10 +153,10 @@
             // btnRefresh
             // 
             btnRefresh.AutoSize = true;
-            btnRefresh.Location = new Point(472, 0);
-            btnRefresh.Margin = new Padding(0, 0, 8, 0);
+            btnRefresh.Location = new Point(480, 0);
+            btnRefresh.Margin = new Padding(0, 0, 10, 0);
             btnRefresh.Name = "btnRefresh";
-            btnRefresh.Size = new Size(90, 31);
+            btnRefresh.Size = new Size(90, 32);
             btnRefresh.TabIndex = 4;
             btnRefresh.Text = "Обновить";
             btnRefresh.UseVisualStyleBackColor = true;
@@ -161,10 +164,10 @@
             // btnApplyRoutes
             // 
             btnApplyRoutes.AutoSize = true;
-            btnApplyRoutes.Location = new Point(570, 0);
+            btnApplyRoutes.Location = new Point(580, 0);
             btnApplyRoutes.Margin = new Padding(0);
             btnApplyRoutes.Name = "btnApplyRoutes";
-            btnApplyRoutes.Size = new Size(148, 31);
+            btnApplyRoutes.Size = new Size(148, 32);
             btnApplyRoutes.TabIndex = 5;
             btnApplyRoutes.Text = "Применить маршруты";
             btnApplyRoutes.UseVisualStyleBackColor = true;
@@ -173,11 +176,12 @@
             // 
             mainPanel.Controls.Add(dgvRules);
             mainPanel.Controls.Add(detailsPanel);
+            mainPanel.BackColor = SystemColors.Control;
             mainPanel.Dock = DockStyle.Fill;
-            mainPanel.Location = new Point(0, 52);
+            mainPanel.Location = new Point(0, 48);
             mainPanel.Name = "mainPanel";
-            mainPanel.Padding = new Padding(10, 0, 10, 10);
-            mainPanel.Size = new Size(1184, 637);
+            mainPanel.Padding = new Padding(12, 0, 12, 12);
+            mainPanel.Size = new Size(1200, 658);
             mainPanel.TabIndex = 1;
             // 
             // dgvRules
@@ -185,16 +189,32 @@
             dgvRules.AllowUserToAddRows = false;
             dgvRules.AllowUserToDeleteRows = false;
             dgvRules.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvRules.BackgroundColor = SystemColors.ControlLightLight;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = SystemColors.Control;
+            dataGridViewCellStyle1.Font = new Font("Segoe UI", 9F);
+            dataGridViewCellStyle1.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
+            dgvRules.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             dgvRules.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvRules.ColumnHeadersHeight = 30;
             dgvRules.Columns.AddRange(new DataGridViewColumn[] { colType, colTarget, colIps, colGateway, colComment, colCreatedAt, colUpdatedAt, colCheckedAt });
             dgvRules.Dock = DockStyle.Fill;
-            dgvRules.Location = new Point(10, 0);
+            dgvRules.EnableHeadersVisualStyles = false;
+            dataGridViewCellStyle2.BackColor = Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = Color.FromArgb(227, 239, 255);
+            dataGridViewCellStyle2.SelectionForeColor = SystemColors.ControlText;
+            dgvRules.DefaultCellStyle = dataGridViewCellStyle2;
+            dgvRules.Location = new Point(12, 0);
             dgvRules.MultiSelect = false;
             dgvRules.Name = "dgvRules";
             dgvRules.ReadOnly = true;
             dgvRules.RowHeadersVisible = false;
+            dgvRules.RowTemplate.Height = 26;
             dgvRules.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-            dgvRules.Size = new Size(1164, 447);
+            dgvRules.Size = new Size(1176, 466);
             dgvRules.TabIndex = 0;
             // 
             // colType
@@ -249,25 +269,25 @@
             // 
             detailsPanel.Controls.Add(bottomLayout);
             detailsPanel.Dock = DockStyle.Bottom;
-            detailsPanel.Location = new Point(10, 447);
+            detailsPanel.Location = new Point(12, 466);
             detailsPanel.Name = "detailsPanel";
-            detailsPanel.Padding = new Padding(0, 10, 0, 0);
-            detailsPanel.Size = new Size(1164, 180);
+            detailsPanel.Padding = new Padding(0, 12, 0, 0);
+            detailsPanel.Size = new Size(1176, 180);
             detailsPanel.TabIndex = 1;
             // 
             // bottomLayout
             // 
             bottomLayout.ColumnCount = 2;
-            bottomLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 68F));
-            bottomLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 32F));
+            bottomLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 70F));
+            bottomLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 30F));
             bottomLayout.Controls.Add(grpDetails, 0, 0);
             bottomLayout.Controls.Add(grpLog, 1, 0);
             bottomLayout.Dock = DockStyle.Fill;
-            bottomLayout.Location = new Point(0, 10);
+            bottomLayout.Location = new Point(0, 12);
             bottomLayout.Name = "bottomLayout";
             bottomLayout.RowCount = 1;
             bottomLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
-            bottomLayout.Size = new Size(1164, 170);
+            bottomLayout.Size = new Size(1176, 168);
             bottomLayout.TabIndex = 0;
             // 
             // grpDetails
@@ -278,7 +298,7 @@
             grpDetails.Margin = new Padding(0, 0, 8, 0);
             grpDetails.Name = "grpDetails";
             grpDetails.Padding = new Padding(10, 8, 10, 10);
-            grpDetails.Size = new Size(783, 170);
+            grpDetails.Size = new Size(815, 168);
             grpDetails.TabIndex = 0;
             grpDetails.TabStop = false;
             grpDetails.Text = "Детали записи";
@@ -286,9 +306,9 @@
             // detailsLayout
             // 
             detailsLayout.ColumnCount = 4;
-            detailsLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 88F));
+            detailsLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 92F));
             detailsLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
-            detailsLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 88F));
+            detailsLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 92F));
             detailsLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
             detailsLayout.Controls.Add(lblTarget, 0, 0);
             detailsLayout.Controls.Add(txtTarget, 1, 0);
@@ -305,19 +325,19 @@
             detailsLayout.Location = new Point(10, 24);
             detailsLayout.Name = "detailsLayout";
             detailsLayout.RowCount = 5;
-            detailsLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
-            detailsLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
-            detailsLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
-            detailsLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
-            detailsLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
-            detailsLayout.Size = new Size(763, 136);
+            detailsLayout.RowStyles.Add(new RowStyle(SizeType.Absolute, 28F));
+            detailsLayout.RowStyles.Add(new RowStyle(SizeType.Absolute, 28F));
+            detailsLayout.RowStyles.Add(new RowStyle(SizeType.Absolute, 28F));
+            detailsLayout.RowStyles.Add(new RowStyle(SizeType.Absolute, 28F));
+            detailsLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            detailsLayout.Size = new Size(795, 134);
             detailsLayout.TabIndex = 0;
             // 
             // lblTarget
             // 
             lblTarget.Anchor = AnchorStyles.Left;
             lblTarget.AutoSize = true;
-            lblTarget.Location = new Point(3, 14);
+            lblTarget.Location = new Point(3, 6);
             lblTarget.Name = "lblTarget";
             lblTarget.Size = new Size(38, 15);
             lblTarget.TabIndex = 0;
@@ -326,16 +346,16 @@
             // txtTarget
             // 
             txtTarget.Anchor = AnchorStyles.Left | AnchorStyles.Right;
-            txtTarget.Location = new Point(91, 11);
+            txtTarget.Location = new Point(95, 3);
             txtTarget.Name = "txtTarget";
-            txtTarget.Size = new Size(284, 23);
+            txtTarget.Size = new Size(300, 23);
             txtTarget.TabIndex = 1;
             // 
             // lblType
             // 
             lblType.Anchor = AnchorStyles.Left;
             lblType.AutoSize = true;
-            lblType.Location = new Point(378, 9);
+            lblType.Location = new Point(401, 6);
             lblType.Name = "lblType";
             lblType.Size = new Size(26, 15);
             lblType.TabIndex = 2;
@@ -347,16 +367,16 @@
             cmbType.DropDownStyle = ComboBoxStyle.DropDownList;
             cmbType.FormattingEnabled = true;
             cmbType.Items.AddRange(new object[] { "IP", "Domain" });
-            cmbType.Location = new Point(469, 6);
+            cmbType.Location = new Point(493, 3);
             cmbType.Name = "cmbType";
-            cmbType.Size = new Size(291, 23);
+            cmbType.Size = new Size(299, 23);
             cmbType.TabIndex = 3;
             // 
             // lblIps
             // 
             lblIps.Anchor = AnchorStyles.Left;
             lblIps.AutoSize = true;
-            lblIps.Location = new Point(3, 42);
+            lblIps.Location = new Point(3, 34);
             lblIps.Name = "lblIps";
             lblIps.Size = new Size(18, 15);
             lblIps.TabIndex = 4;
@@ -366,16 +386,16 @@
             // 
             detailsLayout.SetColumnSpan(txtIps, 3);
             txtIps.Anchor = AnchorStyles.Left | AnchorStyles.Right;
-            txtIps.Location = new Point(91, 39);
+            txtIps.Location = new Point(95, 31);
             txtIps.Name = "txtIps";
-            txtIps.Size = new Size(669, 23);
+            txtIps.Size = new Size(697, 23);
             txtIps.TabIndex = 5;
             // 
             // lblGateway
             // 
             lblGateway.Anchor = AnchorStyles.Left;
             lblGateway.AutoSize = true;
-            lblGateway.Location = new Point(378, 63);
+            lblGateway.Location = new Point(401, 62);
             lblGateway.Name = "lblGateway";
             lblGateway.Size = new Size(42, 15);
             lblGateway.TabIndex = 6;
@@ -384,9 +404,9 @@
             // txtGateway
             // 
             txtGateway.Anchor = AnchorStyles.Left | AnchorStyles.Right;
-            txtGateway.Location = new Point(469, 59);
+            txtGateway.Location = new Point(493, 59);
             txtGateway.Name = "txtGateway";
-            txtGateway.Size = new Size(291, 23);
+            txtGateway.Size = new Size(299, 23);
             txtGateway.TabIndex = 7;
             // 
             // lblComment
@@ -403,17 +423,17 @@
             // 
             detailsLayout.SetColumnSpan(txtComment, 3);
             txtComment.Anchor = AnchorStyles.Left | AnchorStyles.Right;
-            txtComment.Location = new Point(91, 86);
+            txtComment.Location = new Point(95, 87);
             txtComment.Name = "txtComment";
-            txtComment.Size = new Size(669, 23);
+            txtComment.Size = new Size(697, 23);
             txtComment.TabIndex = 9;
             // 
             // btnSave
             // 
-            btnSave.Anchor = AnchorStyles.Right;
-            btnSave.Location = new Point(668, 108);
+            btnSave.Anchor = AnchorStyles.Right | AnchorStyles.Top;
+            btnSave.Location = new Point(696, 115);
             btnSave.Name = "btnSave";
-            btnSave.Size = new Size(92, 28);
+            btnSave.Size = new Size(96, 28);
             btnSave.TabIndex = 10;
             btnSave.Text = "Сохранить";
             btnSave.UseVisualStyleBackColor = true;
@@ -422,11 +442,11 @@
             // 
             grpLog.Controls.Add(rtbLog);
             grpLog.Dock = DockStyle.Fill;
-            grpLog.Location = new Point(791, 0);
+            grpLog.Location = new Point(823, 0);
             grpLog.Margin = new Padding(0);
             grpLog.Name = "grpLog";
             grpLog.Padding = new Padding(10, 8, 10, 10);
-            grpLog.Size = new Size(373, 170);
+            grpLog.Size = new Size(353, 168);
             grpLog.TabIndex = 1;
             grpLog.TabStop = false;
             grpLog.Text = "Лог";
@@ -434,19 +454,21 @@
             // rtbLog
             // 
             rtbLog.Dock = DockStyle.Fill;
+            rtbLog.BackColor = SystemColors.Window;
+            rtbLog.BorderStyle = BorderStyle.FixedSingle;
             rtbLog.Location = new Point(10, 24);
             rtbLog.Name = "rtbLog";
             rtbLog.ReadOnly = true;
-            rtbLog.Size = new Size(353, 136);
+            rtbLog.Size = new Size(333, 134);
             rtbLog.TabIndex = 0;
             rtbLog.Text = "";
             // 
             // statusStrip
             // 
             statusStrip.Items.AddRange(new ToolStripItem[] { lblStatus });
-            statusStrip.Location = new Point(0, 689);
+            statusStrip.Location = new Point(0, 706);
             statusStrip.Name = "statusStrip";
-            statusStrip.Size = new Size(1184, 22);
+            statusStrip.Size = new Size(1200, 22);
             statusStrip.TabIndex = 2;
             // 
             // lblStatus
@@ -459,10 +481,11 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1184, 711);
+            ClientSize = new Size(1200, 728);
             Controls.Add(mainPanel);
             Controls.Add(topPanel);
             Controls.Add(statusStrip);
+            Font = new Font("Segoe UI", 9F);
             MinimumSize = new Size(1000, 650);
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
